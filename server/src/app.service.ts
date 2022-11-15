2ec261e519497a98f14e32f8d318676d95edca76
khRasikh <kh.rasikh542@gmail.com>
2022-11-15 16:54:29 +0100

    initial commit

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
 
 @Injectable()
 export class AppService {
+  constructor() {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  orderPayment(): string {
+    return 'hi';
   }
 }
