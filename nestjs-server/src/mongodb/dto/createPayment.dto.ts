dbff9bf1917582f5db97d692e9eac4a2615bd06e
khRasikh <kh.rasikh542@gmail.com>
2022-12-01 15:41:54 +0100

    feat: adding swagger property

--- nestjs-server/src/mongodb/dto/createPayment.dto.ts
+++ nestjs-server/src/mongodb/dto/createPayment.dto.ts
@@ -1,7 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreatePayment {
+  @ApiProperty()
   id: number;
+
+  @ApiProperty()
   product: string;
+
+  @ApiProperty()
   amount: number;
+
+  @ApiProperty()
   currency: string;
+
+  @ApiProperty()
   date: string;
 }
--- nestjs-server/src/product/dto/createProduct.dto.ts
+++ nestjs-server/src/product/dto/createProduct.dto.ts
@@ -1,10 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateProductDto {
+  @ApiProperty()
   id: number;
+
+  @ApiProperty()
   type: string;
+
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   info: string;
+
+  @ApiProperty()
   location: string;
+
+  @ApiProperty()
   amount: number;
+
+  @ApiProperty()
   currency: string;
   published_date: string;
 }
